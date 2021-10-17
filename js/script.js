36814db7f03e0723aaa82f0737323508bca0e358
alchin_andrey <immortiale@gmail.com>
2021-10-17 18:38:39 +0200

    лк1

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		// autoplay:true,
+		speed:1500,
+		autoplaySpeed:1000,
+		easing:'',
+		// responsive:[
+		// 	{
+		// 		breakpoint: 768,
+		// 		settings: {
+		// 			slidesToShow:1
+		// 		}
+		// 	},
+		// 	{
+		// 		breakpoint: 550,
+		// 		settings: {
+		// 			slidesToShow:1
+		// 		}
+		// 	}
+		// ]
+	});
+});
+
