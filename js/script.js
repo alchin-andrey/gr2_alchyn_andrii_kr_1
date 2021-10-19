9ef5b24315007a0555773d6a42a29bf507850ebb
andrey_alchin <immortiale@gmail.com>
2021-10-19 12:42:52 +0300

    adaptive

--- js/script.js
+++ js/script.js
@@ -3,22 +3,7 @@ $(document).ready(function(){
 		arrows:true,
 		autoplay:true,
 		speed:1500,
-		autoplaySpeed:1000,
-		easing:'',
-		// responsive:[
-		// 	{
-		// 		breakpoint: 768,
-		// 		settings: {
-		// 			slidesToShow:1
-		// 		}
-		// 	},
-		// 	{
-		// 		breakpoint: 550,
-		// 		settings: {
-		// 			slidesToShow:1
-		// 		}
-		// 	}
-		// ]
+		autoplaySpeed:1500,
 	});
 });
 
